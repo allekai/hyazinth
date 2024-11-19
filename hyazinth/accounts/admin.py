a15a34761eae1078c03711eda2ab33508ec86f6d
Alexander Poth <alexander.poth@student.kit.edu>
2024-11-19 23:03:36 +0100

    Add different user type, a user list view for admins

--- hyazinth/accounts/admin.py
+++ hyazinth/accounts/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from .models import User  # Importiere dein User-Modell
 
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
--- hyazinth/hyazinth/settings.py
+++ hyazinth/hyazinth/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
     "fahrten",
     "crispy_forms",
     "crispy_bootstrap5",
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 
 MIDDLEWARE = [
